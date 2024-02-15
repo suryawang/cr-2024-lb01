36e5ef0b7aad5d149439b7e95ede7a13eb94a3b1
Class <Class@C421-075.binus.local>
2024-02-15 17:58:29 +0700

    testing grade

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import university.Grade;
 import university.Student;
+import university.Subject;
 
 public class Main {
 
@@ -7,6 +9,9 @@ public class Main {
         var budi = new Student("1234567894", "Budi");
         budi.setIpk(400);
         System.out.println(budi);
+        var subject = new Subject("S001", "Code Reenginering",4);
+        var grade = new Grade(budi, subject, 85);
+        System.out.println(grade);
      }
 
 }
--- src/university/Grade.java
+++ src/university/Grade.java
@@ -1,5 +1,31 @@
 package university;
 
 public class Grade {
+	private Student student;
+	private Subject subject;
+	private int score;
 
+	public Grade(Student student, Subject subject, int score) {
+		this.student = student;
+		this.subject = subject;
+		this.score = score;
+	}
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public Subject getSubject() {
+		return subject;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public String toString() {
+		return String.format("Student: %s, Subject: %s, Score: %d\n", 
+				student.getStudentId() + "-" + student.getName(),
+				subject.getCode() + "-" + subject.getName(), score);
+	}
 }
