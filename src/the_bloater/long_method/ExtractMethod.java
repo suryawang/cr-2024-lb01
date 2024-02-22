a802da4512c54e27b10a92a7b661e57f84929215
Class <Class@C421-075.binus.local>
2024-02-22 15:41:19 +0700

    long method - treatment extract method

--- src/the_bloater/long_method/ExtractMethod.java
+++ src/the_bloater/long_method/ExtractMethod.java
@@ -23,23 +23,29 @@ public class ExtractMethod {
 
 	// TODO: reduce this method with extract method
 	void printOwing() {
+		printBanner();
+		printDetail(countOwings());
+	}
+
+	private double countOwings() {
 		Enumeration elements = orders.elements();
 		double outstanding = 0.0;
-
-		// print banner
-		System.out.println("*****************************");
-		System.out.println("****** Customer totals ******");
-		System.out.println("*****************************");
-
-		// print owings
 		while (elements.hasMoreElements()) {
 			Order each = (Order) elements.nextElement();
 			outstanding += each.getAmount();
 		}
+		return outstanding;
+	}
 
-		// print details
+	private void printDetail(double outstanding) {
 		System.out.println("name: " + name);
 		System.out.println("amount: " + outstanding);
+	}
+
+	private void printBanner() {
+		System.out.println("*****************************");
+		System.out.println("****** Customer totals ******");
+		System.out.println("*****************************");
 	}
 
 	class Order implements Enumeration {
