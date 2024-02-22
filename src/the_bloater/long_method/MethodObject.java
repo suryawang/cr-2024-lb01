86f6cb6aabdd0198bb9732e5381418fae4637535
Class <Class@C421-075.binus.local>
2024-02-22 16:25:57 +0700

    long method - treatment replace method with method object

--- src/the_bloater/long_method/MethodObject.java
+++ src/the_bloater/long_method/MethodObject.java
@@ -9,16 +9,28 @@ public class MethodObject {
 		// TODO: replace this gamma method with method object (make a class for the
 		// gamma logic)
 		public int gamma(int inputVal, int quantity, int yearToDate) {
-			int importantValue1 = (inputVal * quantity) + delta();
-			int importantValue2 = (inputVal * yearToDate) + 100;
+			return new Gamma(inputVal, quantity, yearToDate, delta()).compute();
+		}
+	}
+
+	class Gamma {
+		private int importantValue1;
+		private int importantValue2;
+		private int importantValue3;
+
+		public Gamma(int inputVal, int quantity, int yearToDate, int delta) {
+			importantValue1 = (inputVal * quantity) + delta;
+			importantValue2 = (inputVal * yearToDate) + 100;
 			if ((yearToDate - importantValue1) > 100) {
 				importantValue2 -= 20;
 			}
-			int importantValue3 = importantValue2 * 7;
+			importantValue3 = importantValue2 * 7;
+		}
+
+		public int compute() {
 			// and so on…
 			return importantValue3 - 2 * importantValue1;
 		}
-
 	}
 
 	private void test() {
