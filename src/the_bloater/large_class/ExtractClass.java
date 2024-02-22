eaaffd785ebde356a0379c22f24bad49b51cfd67
Class <Class@C421-075.binus.local>
2024-02-22 17:30:05 +0700

    large class - treatment extract class

--- src/the_bloater/large_class/ExtractClass.java
+++ src/the_bloater/large_class/ExtractClass.java
@@ -2,33 +2,43 @@ package the_bloater.large_class;
 
 public class ExtractClass {
 	// todo extract class TelephoneNumber from class Person
+	public class TelephoneNumber {
+		public String areaCode;
+		public String number;
+
+		public TelephoneNumber(String areaCode, String number) {
+			this.areaCode = areaCode;
+			this.number = number;
+		}
+
+		public String getTelephoneNumber() {
+			return "(" + areaCode + ") " + number;
+		}
+	}
+
 	class Person {
 		private String name;
-		private String officeAreaCode;
-		private String officeNumber;
+		private TelephoneNumber officeNumber;
+
+		public Person(String name, TelephoneNumber officeNumber) {
+			this.name = name;
+			this.officeNumber = officeNumber;
+		}
 
 		public String getName() {
 			return name;
 		}
 
 		public String getTelephoneNumber() {
-			return ("(" + officeAreaCode + ") " + officeNumber);
+			return officeNumber.getTelephoneNumber();
 		}
 
-		public String getOfficeAreaCode() {
-			return officeAreaCode;
-		}
-
-		public void setOfficeAreaCode(String arg) {
-			officeAreaCode = arg;
-		}
-
-		public String getOfficeNumber() {
+		public TelephoneNumber getOfficeNumber() {
 			return officeNumber;
 		}
 
-		public void setOfficeNumber(String arg) {
-			officeNumber = arg;
+		public void setOfficeNumber(TelephoneNumber number) {
+			officeNumber = number;
 		}
 	}
 }
