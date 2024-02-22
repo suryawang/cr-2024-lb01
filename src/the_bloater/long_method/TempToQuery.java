258ff6547291e1de672af1a15c42f0458dd6ea8e
Class <Class@C421-075.binus.local>
2024-02-22 15:51:54 +0700

    long method - treatment replace temp with query

--- src/the_bloater/long_method/TempToQuery.java
+++ src/the_bloater/long_method/TempToQuery.java
@@ -12,14 +12,17 @@ public class TempToQuery {
 
 		// TODO: change this method to using replace temp with query
 		public double getPrice() {
-			int basePrice = quantity * itemPrice;
-			double discountFactor;
-			if (basePrice > 1000) {
-				discountFactor = 0.95;
-			} else {
-				discountFactor = 0.98;
-			}
-			return basePrice * discountFactor;
+			return basePrice() * getDiscountFactor();
+		}
+
+		private double getDiscountFactor() {
+			if (basePrice() > 1000)
+				return 0.95;
+			return 0.98;
+		}
+
+		private int basePrice() {
+			return quantity * itemPrice;
 		}
 	}
 
