a1f4c73a073a9d2b2fd8465a46d8cab93138f399
Class <Class@C421-075.binus.local>
2024-02-22 15:34:03 +0700

    long method - extract method

---
+++ src/the_bloater/long_method/ExtractMethod.java
@@ -0,0 +1,80 @@
+package the_bloater.long_method;
+
+import java.util.Enumeration;
+
+public class ExtractMethod {
+
+	public static void main(String[] args) {
+		ExtractMethod test = new ExtractMethod("Andi");
+		test.printOwing();
+	}
+
+	private Order orders;
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public ExtractMethod(String name) {
+		this.name = name;
+		orders = new Order();
+	}
+
+	// TODO: reduce this method with extract method
+	void printOwing() {
+		Enumeration elements = orders.elements();
+		double outstanding = 0.0;
+
+		// print banner
+		System.out.println("*****************************");
+		System.out.println("****** Customer totals ******");
+		System.out.println("*****************************");
+
+		// print owings
+		while (elements.hasMoreElements()) {
+			Order each = (Order) elements.nextElement();
+			outstanding += each.getAmount();
+		}
+
+		// print details
+		System.out.println("name: " + name);
+		System.out.println("amount: " + outstanding);
+	}
+
+	class Order implements Enumeration {
+		private double[] amounts;
+		private int currentIndex;
+
+		public Order() {
+			amounts = new double[5];
+			currentIndex = 0;
+			amounts[0] = 12.0;
+			amounts[1] = 2.5;
+			amounts[2] = 3.2;
+			amounts[3] = 7.05;
+			amounts[4] = 6.0;
+		}
+
+		public boolean hasMoreElements() {
+			if (currentIndex < amounts.length) {
+				return true;
+			}
+			return false;
+		}
+
+		public Enumeration elements() {
+			return this;
+		}
+
+		public double getAmount() {
+			return amounts[currentIndex - 1];
+		}
+
+		public Object nextElement() {
+			currentIndex++;
+			return this;
+		}
+
+	}
+}
