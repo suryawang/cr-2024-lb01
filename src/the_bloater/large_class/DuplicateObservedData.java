03ffd126f406ba17444de5bb1271f5f1c02f35fd
Class <Class@C421-075.binus.local>
2024-02-22 18:17:52 +0700

    large class - treatment duplicate observed data

--- src/the_bloater/large_class/DuplicateObservedData.java
+++ src/the_bloater/large_class/DuplicateObservedData.java
@@ -6,19 +6,73 @@ public class DuplicateObservedData {
 	public static void main(String args[]) {
 		new DuplicateObservedData().test();
 	}
+
 	void test() {
 		var a = new IntervalWindow();
 		System.out.println("run");
 	}
+
+	class Interval {
+		private int start;
+		private int end;
+		private int length;
+
+		public int getStart() {
+			return start;
+		}
+
+		public void setStart(String start) {
+			try {
+				this.start = Integer.parseInt(start);
+			} catch (NumberFormatException ex) {
+				this.start = 0;
+			}
+		}
+
+		public int getEnd() {
+			return end;
+		}
+
+		public void setEnd(String end) {
+			try {
+				this.end = Integer.parseInt(end);
+			} catch (NumberFormatException ex) {
+				this.end = 0;
+			}
+		}
+
+		public int getLength() {
+			return length;
+		}
+
+		public void setLength(String length) {
+			try {
+				this.length = Integer.parseInt(length);
+			} catch (NumberFormatException ex) {
+				this.length = 0;
+			}
+		}
+
+		void calculateLength() {
+			length = end - start;
+		}
+
+		void calculateEnd() {
+			end = start + length;
+		}
+	}
+
 	class IntervalWindow extends Frame {
 		java.awt.TextField startField;
 		java.awt.TextField endField;
 		java.awt.TextField lengthField;
+		private Interval interval;
 
 		public IntervalWindow() {
 			startField = new java.awt.TextField();
 			endField = new java.awt.TextField();
 			lengthField = new java.awt.TextField();
+			interval = new Interval();
 			SymFocus focusListener = new SymFocus();
 			startField.addFocusListener(focusListener);
 			endField.addFocusListener(focusListener);
@@ -26,15 +80,6 @@ public class DuplicateObservedData {
 		}
 
 		class SymFocus extends java.awt.event.FocusAdapter {
-			boolean isNotInteger(String text) {
-				try {
-					Integer.parseInt(text);
-					return true;
-				} catch (NumberFormatException ex) {
-					return false;
-				}
-			}
-
 			public void focusLost(java.awt.event.FocusEvent event) {
 				Object object = event.getSource();
 				if (object == startField) {
@@ -47,46 +92,24 @@ public class DuplicateObservedData {
 			}
 
 			void StartField_FocusLost(java.awt.event.FocusEvent event) {
-				if (isNotInteger(startField.getText())) {
-					startField.setText("0");
-				}
-				calculateLength();
+				interval.setStart(startField.getText());
+				startField.setText(interval.getStart() + "");
+				interval.calculateLength();
+				lengthField.setText(String.valueOf(interval.getLength()));
 			}
 
 			void EndField_FocusLost(java.awt.event.FocusEvent event) {
-				if (isNotInteger(endField.getText())) {
-					endField.setText("0");
-				}
-				calculateLength();
+				interval.setEnd(endField.getText());
+				endField.setText(interval.getEnd() + "");
+				interval.calculateLength();
+				lengthField.setText(String.valueOf(interval.getLength()));
 			}
 
 			void LengthField_FocusLost(java.awt.event.FocusEvent event) {
-				if (isNotInteger(lengthField.getText())) {
-					lengthField.setText("0");
-				}
-				calculateEnd();
-			}
-
-			void calculateLength() {
-				try {
-					int start = Integer.parseInt(startField.getText());
-					int end = Integer.parseInt(endField.getText());
-					int length = end - start;
-					lengthField.setText(String.valueOf(length));
-				} catch (NumberFormatException e) {
-					throw new RuntimeException("Unexpected Number Format Error");
-				}
-			}
-
-			void calculateEnd() {
-				try {
-					int start = Integer.parseInt(startField.getText());
-					int length = Integer.parseInt(lengthField.getText());
-					int end = start + length;
-					endField.setText(String.valueOf(end));
-				} catch (NumberFormatException e) {
-					throw new RuntimeException("Unexpected Number Format Error");
-				}
+				interval.setLength(lengthField.getText());
+				lengthField.setText(interval.getLength() + "");
+				interval.calculateEnd();
+				endField.setText(String.valueOf(interval.getEnd()));
 			}
 		}
 	}
