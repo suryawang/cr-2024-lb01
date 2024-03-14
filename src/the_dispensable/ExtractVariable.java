07cd4e301ce7ce008c0973a00be7674971dc3699
Class <Class@C421-075.binus.local>
2024-03-14 15:37:23 +0700

    the dispensable - comments - treatment extract variable

--- src/the_dispensable/ExtractVariable.java
+++ src/the_dispensable/ExtractVariable.java
@@ -5,9 +5,10 @@ public class ExtractVariable {
 	private int itemPrice;
 
 	double price() {
-		// Price consists of: base price - discount + shipping cost
-		return quantity * itemPrice - Math.max(0, quantity - 500) * itemPrice * 0.05
-				+ Math.min(quantity * itemPrice * 0.1, 100.0);
+		final int basePrice = quantity * itemPrice;
+		final double discount = Math.max(0, quantity - 500) * itemPrice * 0.05;
+		final double shippingCost = Math.min(quantity * itemPrice * 0.1, 100.0);
+		return basePrice - discount + shippingCost;
 	}
 
 	void test() {
