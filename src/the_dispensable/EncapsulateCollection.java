84ca0eed1d44496e6ddf77d57e2289bab30b5753
Class <Class@C421-075.binus.local>
2024-03-14 16:55:19 +0700

    the dispensable - data class - treatment encapsulate collection

--- src/the_dispensable/EncapsulateCollection.java
+++ src/the_dispensable/EncapsulateCollection.java
@@ -1,5 +1,6 @@
 package the_dispensable;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
@@ -24,14 +25,17 @@ public class EncapsulateCollection {
 	}
 
 	class Person {
-		private Set courses;
+		private Set courses = new HashSet();
 
 		public Set getCourses() {
-			return courses;
+			return Collections.unmodifiableSet(courses);
 		}
 
-		public void setCourses(Set arg) {
-			courses = arg;
+		public void addCourse(Course course) {
+			courses.add(course);
+		}
+		public void removeCourse(Course course) {
+			courses.remove(course);
 		}
 	}
 
@@ -44,16 +48,21 @@ public class EncapsulateCollection {
 
 	void test() {
 		Person kent = new Person();
-		Set s = new HashSet();
-		s.add(new Course("Smalltalk Programming", false));
-		s.add(new Course("Appreciating Single Malts", true));
-		kent.setCourses(s);
+		kent.addCourse(new Course("Smalltalk Programming", false));
+		kent.addCourse(new Course("Appreciating Single Malts", true));
+//		Set s = new HashSet();
+//		s.add(new Course("Smalltalk Programming", false));
+//		s.add(new Course("Appreciating Single Malts", true));
+//		kent.setCourses(s);
 		Assert.equals(2, kent.getCourses().size());
 		Course refact = new Course("Refactoring", true);
-		kent.getCourses().add(refact);
-		kent.getCourses().add(new Course("Brutal Sarcasm", false));
+		kent.addCourse(refact);
+		kent.addCourse(new Course("Brutal Sarcasm", false));
+//		kent.getCourses().add(refact);
+//		kent.getCourses().add(new Course("Brutal Sarcasm", false));
 		Assert.equals(4, kent.getCourses().size());
-		kent.getCourses().remove(refact);
+		kent.removeCourse(refact);
+//		kent.getCourses().remove(refact);
 		Assert.equals(3, kent.getCourses().size());
 
 		Iterator iter = kent.getCourses().iterator();
